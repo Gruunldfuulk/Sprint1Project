5f25ec1faee38268819375c67163b40f42e16bbc
Alex Juhasz <gruunldfuulk@gmail.com>
2019-02-17 20:43:37 -0500

    Add project files.

---
+++ WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+        public ProductBL()
+        {
+            //
+            // instantiate the view model and initialize the data set
+            //
+
+            _productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            //
+            //
+            //
+            _productWindowView = new ProductWindowView(_productWindowPresenter);
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+    
+        private Product GetProductData()
+        {
+            return new Product()
+            {
+                Id = 0,
+                GameName = "Chrono Trigger",
+                Medium = "ROM Cartridge",
+                ReleaseDate = DateTime.Parse("03/11/1995"),
+                Genre = Product.GenreType.RPG,
+                PersonalRating = 9.5,
+                PurchaseDate = DateTime.Parse("12/15/1995"),
+                Description =
+                "Chrono Trigger is a Role Playing game from the end" +
+                "\nof the SNES. Featuring multiple Timelines that" +
+                "\nthe player must go through. It also makes use " +
+                "\nof some of the first multiple endings, new game+," +
+                "\nactive time battle, and team attacks in the RPG world.",
+                ImageFileName = "snes_chrono_trigger.jpg"
+            };
+        }
+
+        private Company GetCompanyData()
+        {
+            Company company = new Company();
+            company.Name = "Retro Gaming Software, Inc";
+            company.Address = "11 Front Street";
+            company.City = "Traverse City";
+            company.PageLabel = "Game Details";
+
+            return company;
+        }
+        #endregion
+
+        #region METHODS
+
+
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product
+    {
+        #region ENUMS
+
+        public enum GenreType { none, RPG, Beatemup, Action, Adventure, Shooter }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _id;
+        private string _gameName;
+        private string _medium;
+        private DateTime _releaseDate;
+        private GenreType _genre;
+        private string _imageFileName;
+        private string _description;
+        private DateTime _purchaseDate;
+        private double _personalRating;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string GameName
+        {
+            get { return _gameName; }
+            set
+            {
+                _gameName = value;
+               
+            }
+        }
+
+        public string Medium
+        {
+            get { return _medium; }
+            set
+            {
+                _medium = value;
+               
+            }
+        }
+
+        public DateTime ReleaseDate
+        {
+            get { return _releaseDate; }
+            set { _releaseDate = value; }
+        }
+
+        public GenreType Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+
+        public string ImageFileName
+        {
+            get { return _imageFileName; }
+            set { _imageFileName = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public DateTime PurchaseDate
+        {
+            get { return _purchaseDate; }
+            set { _purchaseDate = value; }
+        }
+
+        public double PersonalRating
+        {
+            get { return _personalRating; }
+            set { _personalRating = value; }
+        }
+
+        public string FullName
+        {
+            get { return _gameName + " " + _medium; }
+        }
+
+        public string ImageFilePath
+        {
+            get { return @"../Images/" + _imageFileName; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Product()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
